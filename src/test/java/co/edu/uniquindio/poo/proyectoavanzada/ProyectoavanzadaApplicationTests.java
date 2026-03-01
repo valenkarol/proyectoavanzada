9851baa42a4db7d80c13c9720a916f546d514faf
valentinaG <168238534+valenkarol@users.noreply.github.com>
2026-03-01 12:08:13 -0500

    Initial commit - Proyecto Avanzada

---
+++ src/test/java/co/edu/uniquindio/poo/proyectoavanzada/ProyectoavanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.poo.proyectoavanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoavanzadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
