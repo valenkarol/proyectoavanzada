a9573c35ad8f2d342a180de5501853edccaed574
KarolMD <karolm.davilas@uqvirtual.edu.co>
2026-04-01 12:45:22 -0500

    fix: import Rol en usuario

--- src/main/java/co/edu/uniquindio/poo/proyectoavanzada/Entity/Usuario.java
+++ src/main/java/co/edu/uniquindio/poo/proyectoavanzada/Entity/Usuario.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.poo.proyectoavanzada.Entity;
 
+import co.edu.uniquindio.poo.proyectoavanzada.Enum.Rol;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
